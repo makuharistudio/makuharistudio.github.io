0c960a511a526e4eb475cfe9a8908aeed96c30e3
ｍａｋｕｈａｒｉ <48945612+makuharistudio@users.noreply.github.com>
2026-01-12 19:35:39 +1100

    Updated design for site header menu

--- src/components/MenuHeader.jsx
+++ src/components/MenuHeader.jsx
@@ -1,19 +1,63 @@
 import { useLocation } from 'react-router-dom';
 import { HashLink as Link } from 'react-router-hash-link';
+import {
+  menu_about, menu_project, menu_blog, menu_reading,
+  menu_about_active, menu_project_active, menu_blog_active, menu_reading_active,
+} from '../data/assets';
 
 export default function MenuHeader() {
 
   const location = useLocation();
   const currentPath = location.pathname;
 
+  const linkConfigs = [
+    {
+      to: '/',
+      label: 'ABOUT',
+      isActive: currentPath === '/',
+      iconDefault: menu_about,
+      iconActive: menu_about_active,      
+      lightActive: 'menu-header-light-highlight-red',
+    },
+    {
+      to: '/portfolio',
+      label: 'PROJECT',
+      isActive: currentPath.includes('/portfolio'),
+      iconDefault: menu_project,
+      iconActive: menu_project_active,
+      lightActive: 'menu-header-light-highlight-purple',
+    },
+    {
+      to: '/blog',
+      label: 'BLOG',
+      isActive: currentPath.includes('/blog'),
+      iconDefault: menu_blog,
+      iconActive: menu_blog_active,
+      lightActive: 'menu-header-light-highlight-yellow',
+    },
+    {
+      to: '/readings',
+      label: 'READING',
+      isActive: currentPath.includes('/readings'),
+      iconDefault: menu_reading,
+      iconActive: menu_reading_active,
+      lightActive: 'menu-header-light-highlight-blue',
+    },
+  ];
+
   return (
     <nav id='menu-header'>
-        <ul>
-            <li className={currentPath === '/' ? 'menu-header-highlight' : 'menu-header'}><Link to='/'>ABOUT</Link></li>
-            <li className={currentPath.includes('/portfolio') ? 'menu-header-highlight' : 'menu-header'}><Link to='/portfolio'>PORTFOLIO</Link></li>
-            <li className={currentPath.includes('/blog') ? 'menu-header-highlight' : 'menu-header'}><Link to='/blog'>BLOG</Link></li>
-            <li className={currentPath.includes('/readings') ? 'menu-header-highlight' : 'menu-header'}><Link to='/readings'>READINGS</Link></li>
-        </ul>
+      {linkConfigs.map((link, index) => (
+        <Link key={index} to={link.to}>
+          <div className='menu-header-button'>
+            {link.iconDefault && (
+              <img src={link.isActive ? link.iconActive : link.iconDefault} alt={link.label}/>)}
+          </div>
+          <div className='menu-header-label'>
+            <h3 className={link.isActive ? 'menu-header' : ''}>{link.label}</h3>
+          </div>
+        </Link>
+      ))}
     </nav>
   )
 }
